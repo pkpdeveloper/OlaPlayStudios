659cceae87fe3f360a2c2823199d8a25c9758617
Pankaj Kant Patel <pkpdeveloper@gmail.com>
2017-12-16 23:05:19 +0530

    add runtime permission check using dexter library

--- app/src/main/java/com/ola/playstudios/ui/main/MainActivity.kt
+++ app/src/main/java/com/ola/playstudios/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ola.playstudios.ui.main
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,6 +8,14 @@ import android.view.Menu
 import android.view.MenuItem
 import com.ola.playstudios.R
 import com.ola.playstudios.ui.history.HistoryActivity
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import android.Manifest.permission
+import android.Manifest.permission.RECORD_AUDIO
+import android.Manifest.permission.READ_CONTACTS
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionRequest
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +23,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //check runtime permissions
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.INTERNET
+                ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+
+            }
+
+            override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {/* ... */
+            }
+        }).check()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
