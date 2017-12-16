143ab62fcc95a1ac2324b50412adc3c8a76cb852
Pankaj Kant Patel <pkpdeveloper@gmail.com>
2017-12-16 22:58:24 +0530

    add song download functionality

--- app/src/main/java/com/ola/playstudios/ui/main/MainFragment.kt
+++ app/src/main/java/com/ola/playstudios/ui/main/MainFragment.kt
@@ -1,7 +1,9 @@
 package com.ola.playstudios.ui.main
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -9,13 +11,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import com.koushikdutta.ion.Ion
 import com.ola.playstudios.R
 import com.ola.playstudios.adapter.SongsRecycleViewAdapter
 import com.ola.playstudios.data.SongData
 import com.ola.playstudios.listener.SongsItemClickListener
 import com.ola.playstudios.ui.player.PlayerActivity
 import kotterknife.bindView
-import java.io.Serializable
+import java.io.File
+import android.widget.Toast
+import android.content.ActivityNotFoundException
+import android.net.Uri
+import android.support.v4.content.ContextCompat.startActivity
+import android.webkit.MimeTypeMap
+
 
 /**
  * Created by pankaj on 16/12/17.
@@ -24,6 +33,7 @@ class MainFragment : Fragment(), MainView, SongsItemClickListener {
 
     val songsRecycleView: RecyclerView by bindView(R.id.songsRecycleView)
     val progressBar: ProgressBar by bindView(R.id.progressBar)
+    val downloadProgressBar: ProgressBar by bindView(R.id.downloadProgressBar)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
@@ -69,9 +79,81 @@ class MainFragment : Fragment(), MainView, SongsItemClickListener {
     }
 
     override fun onDownloadButtonClicked(songData: SongData?) {
+        val targetFile = getLocalFile(songData?.song)
+        downloadProgressBar.isIndeterminate = true
+        downloadProgressBar.visibility = View.VISIBLE
+        Ion.with(this)
+                .load(songData?.url)
+                .progress { downloaded, total ->
+                    val progress = downloaded / total * 100.0f
+                    downloadProgressBar.setProgress(progress.toInt())
+                }
+                .write(targetFile)
+                .setCallback({ e, downloadedFile ->
+
+                    if (downloadedFile.exists()) {
+                        downloadProgressBar.visibility = View.INVISIBLE
+                        showAlert(downloadedFile)
+                    }
+
+                })
+    }
+
+    private fun showAlert(downloadedFile: File?) {
+        val alertBuilder = AlertDialog.Builder(context)
+        alertBuilder.setMessage("Song downloaded successfully!")
+        alertBuilder.setPositiveButton("Open", { dialogInterface, k ->
+            dialogInterface.dismiss()
+            // openFileIntent(downloadedFile)
+            Toast.makeText(context, "File saved at "+downloadedFile?.path, Toast.LENGTH_LONG).show()
+        })
+        alertBuilder.setNegativeButton("Cancel", null)
+        alertBuilder.create().show()
+    }
+
+    private fun openFileIntent(downloadedFile: File?) {
+        val myMime = MimeTypeMap.getSingleton()
+        val newIntent = Intent(Intent.ACTION_VIEW)
+        val mimeType = myMime.getMimeTypeFromExtension(fileExt(downloadedFile?.absolutePath!!)?.substring(1))
+        newIntent.setDataAndType(Uri.fromFile(downloadedFile), mimeType)
+        newIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        try {
+            context?.startActivity(newIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, "No handler for this type of file.", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun getLocalFile(songName: String?): File? {
+        val file = File(Environment.getExternalStorageDirectory(), songName + ".mp3")
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        return file
     }
 
     override fun onError(message: String) {
 
+    }
+
+    private fun fileExt(url: String): String? {
+        var url = url
+        if (url.indexOf("?") > -1) {
+            url = url.substring(0, url.indexOf("?"))
+        }
+        if (url.lastIndexOf(".") == -1) {
+            return null
+        } else {
+            var ext = url.substring(url.lastIndexOf(".") + 1)
+            if (ext.indexOf("%") > -1) {
+                ext = ext.substring(0, ext.indexOf("%"))
+            }
+            if (ext.indexOf("/") > -1) {
+                ext = ext.substring(0, ext.indexOf("/"))
+            }
+            return ext.toLowerCase()
+
+        }
     }
 }
